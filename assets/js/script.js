7d0ddcdc4ecd6658ce26163d6664329f8df5e370
Seong Cho <connorcho22@gmail.com>
2023-01-30 19:38:53 -0500

    pushed wrong files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,98 +1,133 @@
-var apiKey = "f61c25ccc3ebc66abfbc574449b8e000";
-var cityName;
-var searchCity = "";
-var cityWeather = [];
+var city="";
+var searchCity = $("#search-city");
+var searchButton = $("#search-button");
+var clearButton = $("#clear-history");
+var currentCity = $("#current-city");
+var currentTemperature = $("#temperature");
+var currentHumidty= $("#humidity");
+var currentWSpeed=$("#wind-speed");
+var currentUvindex= $("#uv-index");
+var sCity=[];
+
+var APIKey="2092386cac3af1026c924a0c78823201";
 
 
-$("#search-button").on("click", function (event) {
+function displayWeather(event){
     event.preventDefault();
-    searchCity = $("#search-city").val().trim();
-    console.log(searchCity);
-    cityWeather.push(searchCity);
-    getWeatherByCity()
-    renderCityBtn();
-
-});
-
-function getWeatherByCity() {
-    var weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=${apiKey}`;
+    if(searchCity.val().trim()!==""){
+        city=searchCity.val().trim();
+        currentWeather(city);
+    }
+}
+// Here we create the AJAX call
+function currentWeather(city){
+    // Here we build the URL so we can get a data from server side.
+    var queryURL= "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + APIKey;
     $.ajax({
-        url: weatherUrl,
-        method: "GET"
-    }).then(function (response) {
+        url:queryURL,
+        method:"GET",
+    }).then(function(response){
 
-        var lat = response.coord.lat;
-        var lon = response.coord.lon;
-        var cityName = searchCity + "";
-        var tempF = ((response.main.temp - 273.15) * 1.80 + 32).toFixed();
-        var humidity = response.main.humidity;
-        var windSpeed = response.wind.speed;
-        var icon = response.weather[0].icon;
-        var iconUrl = `http://openweathermap.org/img/wn/${icon}.png`;
+        // parse the response to display the current weather including the City name. the Date and the weather icon. 
+        console.log(response);
+        //Dta object from server side Api for icon property.
+        var weathericon = response.weather[0].icon;
+        var iconurl = "https://openweathermap.org/img/wn/"+weathericon +"@2x.png";
+        // The date format method is taken from the  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
+        var date = new Date(response.dt*1000).toLocaleDateString();
+        //parse the response for name of city and concanatig the date and icon.
+        $(currentCity).html(response.name +"("+date+")" + "<img src="+iconurl+">");
+        // parse the response to display the current temperature.
+        // Convert the temp to fahrenheit
 
-        $("#temperature").text(tempF + "°F");
-        $("#humidity").text(humidity + "%");
-        $("#windspeed").text(windSpeed + " MPH");
-        $("#city").text(cityName + ",  ");
-        var date = new Date().toLocaleString();
-        $("#date").text(date + " ");
-        $("#img").attr("src", iconUrl);
-
-        var queryLat = lat;
-        var queryLon = lon;
-
-        // API call to get the UV values.
-        var uvIndexUrl = `https://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${queryLat}&lon=${queryLon}`;
-        $.ajax({
-            url: uvIndexUrl,
-            method: "GET"
-        }).then(function (response2) {
-
-            var uvValue = response2.value;
-            $("#uv").text(uvValue);
-            var i = uvValue;
-
-            if (i < 3) {
-                $("#uv").addClass("green");
-            } else if (i < 6) {
-                $("#uv").addClass("yellow");
-            } else if (i < 8) {
-                $("#uv").addClass("orange");
-            } else if (i < 11) {
-                $("#uv").addClass("red");
-            } else {
-                $("#uv").addClass("purple");
+        var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+        $(currentTemperature).html((tempF).toFixed(2)+"&#8457");
+        // Display the Humidity
+        $(currentHumidty).html(response.main.humidity+"%");
+        //Display Wind speed and convert to MPH
+        var ws=response.wind.speed;
+        var windsmph=(ws*2.237).toFixed(1);
+        $(currentWSpeed).html(windsmph+"MPH");
+        // Display UVIndex.
+        //By Geographic coordinates method and using appid and coordinates as a parameter we are going build our uv query url inside the function below.
+        UVIndex(response.coord.lon,response.coord.lat);
+        forecast(response.id);
+        if(response.cod==200){
+            sCity=JSON.parse(localStorage.getItem("cityname"));
+            console.log(sCity);
+            if (sCity==null){
+                sCity=[];
+                sCity.push(city.toUpperCase()
+                );
+                localStorage.setItem("cityname",JSON.stringify(sCity));
+                addToList(city);
             }
-        })
-        
-        //  This API call returns the data for the 5-Day forecast and populates the cards.
-        var fiveDayUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${queryLat}&lon=${queryLon}&exclude=current,minutely,hourly&appid=${apiKey}`;
-        $.ajax({
-            url: fiveDayUrl,
-            method: "GET"
-        }).then(function (response3) {
-            $(".five-day").each(function (index) {
-                var Resdata = response3.daily[index + 1];
-                var dateElem = Resdata.dt;
-                var day = moment.unix(dateElem).format('l');
-                var icon = Resdata.weather[0].icon;
-                var iconUrl = `http://openweathermap.org/img/wn/${icon}.png`;
-                var temp = ((Resdata.temp.day - 273.15) * 1.80 + 32).toFixed();
-                var humidity = Resdata.humidity;
+            else {
+                if(find(city)>0){
+                    sCity.push(city.toUpperCase());
+                    localStorage.setItem("cityname",JSON.stringify(sCity));
+                    addToList(city);
+                }
+            }
+        }
 
-                $(this).find(".date").text(day);
-                $(this).find(".weather-icon").attr("src", iconUrl);
-                $(this).find(".temperature").text(temp + "°F");
-                $(this).find(".humidity").text(humidity + "%");
-            })
-
-        });
     });
 }
 
-function renderCityBtn() {
-    for (var i = 0; i < cityWeather.length; i++) {
-        var cityBtn = $("<button>").text(cityWeather[i]);
-        $(".button-storage").append(cityBtn);
-    }
+function UVIndex(ln,lt){
+    //lets build the url for uvindex.
+    var uvqURL="https://api.openweathermap.org/data/2.5/uvi?appid="+ APIKey+"&lat="+lt+"&lon="+ln;
+    $.ajax({
+            url:uvqURL,
+            method:"GET"
+            }).then(function(response){
+                $(currentUvindex).html(response.value);
+            });
 }
+    
+// Here we display the 5 days forecast for the current city.
+function forecast(cityid){
+    var dayover= false;
+    var queryforcastURL="https://api.openweathermap.org/data/2.5/forecast?id="+cityid+"&appid="+APIKey;
+    $.ajax({
+        url:queryforcastURL,
+        method:"GET"
+    }).then(function(response){
+        
+        for (i=0;i<5;i++){
+            var date= new Date((response.list[((i+1)*8)-1].dt)*1000).toLocaleDateString();
+            var iconcode= response.list[((i+1)*8)-1].weather[0].icon;
+            var iconurl="https://openweathermap.org/img/wn/"+iconcode+".png";
+            var tempK= response.list[((i+1)*8)-1].main.temp;
+            var tempF=(((tempK-273.5)*1.80)+32).toFixed(2);
+            var humidity= response.list[((i+1)*8)-1].main.humidity;
+        
+            $(".fDate"+i).html(date);
+            $(".fImg"+i).html("<img src="+iconurl+">");
+            $(".fTemp"+i).html(tempF+"&#8457");
+            $(".fHumidity"+i).html(humidity+"%");
+        }
+        
+    });
+}
+
+
+
+// render function
+function loadlastCity(){
+    $("ul").empty();
+    var sCity = JSON.parse(localStorage.getItem("cityname"));
+    if(sCity!==null){
+        sCity=JSON.parse(localStorage.getItem("cityname"));
+        for(i=0; i<sCity.length;i++){
+            addToList(sCity[i]);
+        }
+        city=sCity[i-1];
+        currentWeather(city);
+    }
+
+}
+$("#search-button").on("click",displayWeather);
+$(document).on("click",invokePastSearch);
+$(window).on("load",loadlastCity);
+$("#clear-history").on("click",clearHistory);
